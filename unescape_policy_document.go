edf6e074f20dc5b6dff023f25ca7f527e6c5be93
utgwkk <utagawakiki@gmail.com>
2024-11-30 12:55:57 +0900

    Add references to AWS API document

--- unescape_policy_document.go
+++ unescape_policy_document.go
@@ -6,6 +6,11 @@ import (
 )
 
 func unescapePolicyDocument(policyDocument string) (string, error) {
+	// A policy document obtained from GetRolePolicy or GetPolicyVersion is URL-encoded.
+	//
+	// refs:
+	// - https://docs.aws.amazon.com/IAM/latest/APIReference/API_GetRolePolicy.html
+	// - https://docs.aws.amazon.com/IAM/latest/APIReference/API_GetPolicyVersion.html
 	unescaped, err := url.QueryUnescape(policyDocument)
 	if err != nil {
 		return "", fmt.Errorf("unescapePolicyDocument: %w", err)
